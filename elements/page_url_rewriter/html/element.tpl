cc55421adb31c7449b27f480f0f0375037c50772
Benjamin Dean <ben@benjamindean.com>
2017-10-20 16:13:21 -0700

    Provide new example that shows using a fragment identifier in response to developer reply-question

--- elements/page_url_rewriter/html/element.tpl
+++ elements/page_url_rewriter/html/element.tpl
@@ -2,4 +2,8 @@
 
     <input type="button" id="pusher" value="{{buttonDisplayValue}}">
 
+    <input type="button" id="hasher" value="{{hasherDisplayValue}}">
+
+    <div id="categoryContent" style="display: none;">This should only be visible once we click on the button.</div>
+
 </div>
--- elements/page_url_rewriter/js/element.js
+++ elements/page_url_rewriter/js/element.js
@@ -1,15 +1,26 @@
 PlatformElement.extend({
     initialize: function() {
         let pusherButton = document.querySelector('#pusher');
+        let hasherButton = document.querySelector('#hasher');
+        
+        // If the page is loaded with a hash, display the right stuff
+        if(window.location.hash) {
+            console.log('We have a hash!');
+            if('#myCategoryId' === window.location.hash) {
+                console.log('Ooh, and it matches our category, we should display the category contents now');
+                document.querySelector('#categoryContent').style.display = block;
+            }
+        }
         console.log('Current state: ', window.history.state);
     },
 
     events: {
-        'click #pusher': 'pushState'
+        'click #pusher': 'pushState',
+        'click #hasher': 'hashState'
     },
 
     pushState: function(evt) {
-        console.log('Button Click Event!');
+        console.log('Change History Button Click Event!');
         //if('page/subpage.html' === window.history.state) return; // Don't add extra duplicate history
         let stateObj = {page: "subpage"};
         let title = 'Sub Page';
@@ -17,5 +28,16 @@ PlatformElement.extend({
 
         window.history.pushState(stateObj, title, url);
         return console.log('User modified window.history, appended `page/subpage.html`');
+    },
+
+
+    hashState: function(evt) {
+        console.log('Append Hash History Button Click Event!');
+        let stateObj = {category: "myCategoryId"};
+        let title = 'Shop - Some Category Display Title;
+        let url = 'shop.html#myCategoryId';
+
+        window.history.pushState(stateObj, title, url);
+        return console.log('New state: ', window.history.state);
     }
 });
